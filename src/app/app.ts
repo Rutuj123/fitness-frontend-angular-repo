0e715edca038c19f73d3d03b4339d0a14b773814
rutuja <rutujakolhe29@gmail.com>
2026-05-19 11:35:06 +0530

    change in payment.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'SmartGymManagement_FrontEnd';
+  protected title = 'FitnessHUb';
 }
